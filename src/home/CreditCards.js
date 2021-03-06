f2c414a3de528efedc6b737f08d53a76ad2dfcda
Nick Snyder <nsnyder1992@gmail.com>
2021-03-06 09:49:35 -0500

    added more loading feedback to Credit Cards and Are You sure

--- src/home/CreditCards.js
+++ src/home/CreditCards.js
@@ -4,11 +4,14 @@ import CreditCard from "./CreditCard";
 
 //get base url of backend
 import { BASEURL } from "../context/base-url-context";
+import { CircularProgress, ListItem, ListItemText } from "@material-ui/core";
 
 const CreditCards = ({ sessionToken }) => {
   const [cards, setCards] = useState();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     fetch(`${BASEURL}/stripe/customer/payment/methods`, {
       method: "GET",
       headers: new Headers({
@@ -16,12 +19,20 @@ const CreditCards = ({ sessionToken }) => {
       }),
     })
       .then((res) => res.json())
-      .then((json) => setCards(json.paymentMethods.data))
-      .catch((err) => {});
+      .then((json) => {
+        setLoading(false);
+        setCards(json.paymentMethods.data);
+      })
+      .catch((err) => setLoading(false));
   }, [sessionToken]);
 
   return (
     <div>
+      {loading ? (
+        <ListItem>
+          <CircularProgress />
+        </ListItem>
+      ) : null}
       {cards?.map((card) => {
         return <CreditCard card={card} sessionToken={sessionToken} />;
       })}
